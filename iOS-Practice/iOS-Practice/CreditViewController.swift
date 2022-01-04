fd04165d5dfaba440464cbc60f9dd0809d2abd34
KingDonggyu <dgkim69166916@gmail.com>
2022-01-04 18:24:28 +0900

    Remove .git files

---
+++ iOS-Practice/iOS-Practice/CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  iOS-Practice
+//
+//  Created by 김동규 on 2022/01/04.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
